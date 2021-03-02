e2c1c4f6a0d4ce4cbc4d4dcb52a8e7f62032c1c5
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-03-02 09:00:13 +0000

    .

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").hide();
+        $(".stream1").show('slow');
+        $(".stream1").show('medium');
+        $(".stream1").show('fast');
+        $(".stream1").show(1000);
+
+
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
